61b460b34b3c014fd6c1fa74e22d623b5a5c6efe
Ashish Makwana <85098548+ashishmakwana96am@users.noreply.github.com>
2025-04-11 15:57:30 +0530

    code

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+  const buttons = document.querySelectorAll(".tab-btn");
+
+  buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      buttons.forEach(b => b.classList.remove("active"));
+      btn.classList.add("active");
+    });
+  });
